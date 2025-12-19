c15c88a6ebd7e5fa7739ad026b06afa4265e260b
Fidel Castro <fidelcastro4080@gmail.com>
2025-12-19 13:25:07 +0300

    added JS file

---
+++ index.js
@@ -0,0 +1,27 @@
+//A Program To Shuffle Arrays
+
+//Fisher-Yates algorithm 
+
+const myArray = ['A', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'B', 'F', 'v' ]; 
+
+
+function shuffle(myArray){
+    for(let i = myArray.length - 1; i > 0; i--){
+        let random = Math.floor(Math.random() * (i + 1));
+        [myArray[i], myArray[random]] = [myArray[random], myArray[i]];   // Also the contents in the for loop can be written as.....
+
+    /*    let temp = myArray[i];
+        myArray[i] = myArray[random];
+        myArray[random] = temp;       
+    */    
+
+    }
+}
+
+shuffle(myArray);
+console.log(myArray);
+
+
+
+
+
